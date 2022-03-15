bf9042985f8dcb92ef5eb3d44caf0e817f7a92a5
spancavil <soancavil@gmail.com>
2022-03-15 20:21:28 -0300

    Commit previo clase 2

--- Clase 2 - Instalación y configuración del entorno/Más básicos JS/examples.js
+++ Clase 2 - Instalación y configuración del entorno/Más básicos JS/examples.js
@@ -1,11 +1,4 @@
-//Destructuring arrays
-
-//Destructuring objetos
-
-//Ejemplo de map (devolución de otro array) y diferencia con forEach (procesamiento)
-
 //Spread operator, uso para combinar objetos y arrays
-
 //Uso para copiar sin mutar al ejemplo original (muy útil en React)
 const array1 = ["hola", "qué", "tal"]
 
@@ -20,3 +13,11 @@ const objeto1 = {nombre: "Sebas", apellido: "Ancavil"}
 const objeto2 = {...objeto1, edad: 32}
 
 console.log(objeto1, objeto2);
+
+//Destructuring arrays
+
+//Destructuring objetos
+
+//Ejemplo de map (devolución de otro array) y diferencia con forEach (procesamiento)
+
+
