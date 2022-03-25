60e8acddbfe4727443bf67534669a35663604709
spancavil <soancavil@gmail.com>
2022-03-25 11:11:46 -0300

    Desarrollo clase 5 II

--- Clase 5 - Promises asincronía y map/after-hasta-clase-4/src/containers/ItemListContainer/index.jsx
+++ Clase 5 - Promises asincronía y map/after-hasta-clase-4/src/containers/ItemListContainer/index.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
-import ItemCount from '../../components/ItemCount'
+import React, { useEffect, useState } from 'react';
 
 const ItemListContainer = ({greeting}) => {
 
-    const onAdd = (count) => {
+    const [products, setProducts] = useState([]);
+
+    /* const onAdd = (count) => {
         console.log(`Se agregaron ${count} productos al carrito`);
-    }
+    } */
+
+    useEffect(()=> {
+        //IIFE
+        ( async ()=> {
+            try {
+                const response = await fetch('/data.json');
+                console.log(response);
+                const data = await response.json();
+                console.log(data);
+                setProducts(data);
+            } catch (error) {
+                console.log(error);
+            }
+        })()
+
+    }, [])
+
+    console.log(products);
 
     return (
         <div>
             <h2>{greeting}</h2>
-            <ItemCount stock={10} onAdd={onAdd}/>
-            <ItemCount stock={15} onAdd={onAdd}/>
+            {/* NOTA: Llamar a ItemList para que haga el map */}
+            <ul>
+                {products.map(product => {
+                    return <li>{product.title}</li>
+                })}
+            </ul>
         </div>
     )
 }
