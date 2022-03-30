49b0c7046ef238be92e8012e484379731ffb1e83
spancavil <soancavil@gmail.com>
2022-03-30 10:46:35 -0300

    Route not found

--- Clase 7 - Routing y Navegación/clase7-project/src/App.js
+++ Clase 7 - Routing y Navegación/clase7-project/src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar';
 import ItemDetailContainer from './containers/ItemDetailContainer';
 import ItemListContainer from './containers/ItemListContainer';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path = '/' element = {<ItemListContainer/>}/>
         <Route path = '/category/:id' element = {<ItemListContainer/>} />
         <Route path = '/item/:id' element = {<ItemDetailContainer/>} />
+        <Route path = '*' element = {<NotFound/>} />
       {/* <ItemListContainer greeting={"Bienvenidos a nuestra after app"}/> */}
       {/* <ItemDetailContainer/> */}
       </Routes>
