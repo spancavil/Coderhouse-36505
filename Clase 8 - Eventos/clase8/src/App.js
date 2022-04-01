4bdd3714415bb40e040601042821962acab3a984
spancavil <soancavil@gmail.com>
2022-04-01 11:03:24 -0300

    Desarrollo clase 8 eventos III

--- Clase 8 - Eventos/clase8/src/App.js
+++ Clase 8 - Eventos/clase8/src/App.js
@@ -4,14 +4,14 @@ import ItemDetailContainer from './containers/ItemDetailContainer';
 import ItemListContainer from './containers/ItemListContainer';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import NotFound from './components/NotFound';
-import TestContainer from './containers/TestContainer';
+// import TestContainer from './containers/TestContainer';
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar/>
       <Routes>
-        <Route path = '/' element = {<TestContainer/>}/>
+        <Route path = '/' element = {<ItemListContainer/>}/>
         <Route path = '/category/:id' element = {<ItemListContainer/>} />
         <Route path = '/item/:id' element = {<ItemDetailContainer/>} />
         <Route path = '*' element = {<NotFound/>} />
--- Clase 8 - Eventos/clase8/src/components/ItemDetail/index.jsx
+++ Clase 8 - Eventos/clase8/src/components/ItemDetail/index.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React from 'react';
+import ItemCount from '../ItemCount';
 
 const ItemDetail = ({personaje}) => {
+
+    const [quantity, setQuantity] = React.useState(0)
+
+    const handleAdd = (quantity) => {
+        console.log(quantity);
+        setQuantity(quantity);
+    }
+
+    const handleTerminate = () => {
+        console.log("Terminó la compra")
+    }
+
     return (
         <div style={{
             display: 'flex',
@@ -19,6 +32,11 @@ const ItemDetail = ({personaje}) => {
             <div>
                 <h2>Name: {personaje.name}</h2>
                 <h2>Species: {personaje.species}</h2>
+                {quantity === 0 ? 
+                    <ItemCount stock={10} onAdd={handleAdd}/> 
+                    :
+                    <button className="btn btn-primary" onClick={handleTerminate}>Finalizar compra</button>     
+                }
             </div>
         </div>
     )
