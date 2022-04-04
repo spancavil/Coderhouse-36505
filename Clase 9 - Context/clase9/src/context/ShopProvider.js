5c1f23a70b9824bb3271aee9f07d165cfdd6d588
spancavil <soancavil@gmail.com>
2022-04-04 11:01:00 -0300

    Desarrollo Clase 9 IV

--- Clase 9 - Context/clase9/src/context/ShopProvider.js
+++ Clase 9 - Context/clase9/src/context/ShopProvider.js
@@ -1,8 +1,8 @@
-import {createContext, useState} from 'react';
+import { createContext, useState } from 'react';
 
 export const Shop = createContext();
 
-const ShopProvider = ({children}) => {
+const ShopProvider = ({ children }) => {
 
     /* const [estadoA, setEstadoA] = useState("Estado inicial de A");
     const [estadoB, setEstadoB] = useState("Estado inicial de B"); */
@@ -11,31 +11,31 @@ const ShopProvider = ({children}) => {
         return (a ** b);
     } */
 
-    const [cart, setCart] = useState([])
+    const [cart, setCart] = useState([]);
 
     const addCart = (product, quantityToAdd) => {
-        
+
         const producto = isInCart(product);
         console.log(producto);
         if (producto) {
-            producto.quantity += quantityToAdd
+            producto.quantity += quantityToAdd;
             const cartFiltrado = cart.filter(elemento => elemento.id !== producto.id);
-            cartFiltrado.push(producto)
-            setCart(cartFiltrado)
+            cartFiltrado.push(producto);
+            setCart(cartFiltrado);
             //Deberíamos agregar la cantidad al producto existente
         } else {
             //Agregamos un nuevo objeto al carrito
-            setCart([...cart, {...product, quantity: quantityToAdd}])
+            setCart([...cart, { ...product, quantity: quantityToAdd }]);
         }
 
     }
     //Función auxiliar que me determina si el producto está o no en el cart
     const isInCart = (producto) => {
-        return cart.find(elemento => elemento.id === producto.id)
+        return cart.find(elemento => elemento.id === producto.id);
     }
 
     return (
-        <Shop.Provider value = {{
+        <Shop.Provider value={{
             addCart
         }}>
             {children}
