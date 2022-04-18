d565eaab4079467fda0e47c311202008d2b6de19
spancavil <soancavil@gmail.com>
2022-04-18 11:09:50 -0300

    Desarrollo clase 14

---
+++ Clase 14 - Iniciando con React Native/clase-14/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, Text} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Clase 14 - Introducción a React Native</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0.5,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+  },
+  text: {
+    color: '#fff'
+  }
+});
