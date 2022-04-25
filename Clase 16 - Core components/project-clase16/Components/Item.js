164be494788fbbec230dd1c98cff4488886c46d4
spancavil <soancavil@gmail.com>
2022-04-25 10:13:59 -0300

    Desarrollo Clase 17 III

--- Clase 16 - Core components/project-clase16/Components/Item.js
+++ Clase 16 - Core components/project-clase16/Components/Item.js
@@ -1,13 +1,31 @@
-import {Text, View, StyleSheet} from "react-native"
+import { useState } from "react";
+import {Text, View, StyleSheet, Modal, Touchable, TouchableOpacity, Button} from "react-native"
 import { colors } from "../Styles/Global"
 
 const Item = ({todo}) => {
+
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
-    <View style={styles.todoContainer}>
-      <Text style={styles.text}>
-        {todo}
-      </Text>
-    </View>
+    <>
+    <TouchableOpacity onPress={()=> setModalVisible(true)}>
+      <View style={styles.todoContainer}>
+        <Text style={styles.text}>
+          {todo}
+        </Text>
+      </View>
+    </TouchableOpacity>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={()=> setModalVisible(false)}
+      >
+        <View style={styles.modal}>
+          <Button title="Cerrar modal" onPress={()=>setModalVisible(false)} />
+        </View>
+      </Modal>
+    </>
   )
 }
 
@@ -22,5 +40,12 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 18,
+  },
+  modal: {
+    backgroundColor: '#fff',
+    width: 300,
+    height: 400,
+    marginTop: 100,
+    marginLeft: 50
   }
 })
