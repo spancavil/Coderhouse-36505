936841c4ecc72a1798182adee75be51a2f3924f0
spancavil <soancavil@gmail.com>
2022-04-29 10:50:11 -0300

    Desarrollo clase 19 IV

--- Clase 19 - Auth/Screens/Auth.js
+++ Clase 19 - Auth/Screens/Auth.js
@@ -12,42 +12,46 @@ const Auth = () => {
     const [loginView, setLoginView] = useState(false);
 
     const handleSignup = () => {
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                // Signed in 
-                console.log(userCredential)
-                const user = userCredential.user;
-                // ...
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.log(errorCode, errorMessage);
-                // ..
-            })
-            .finally(()=> {
-                setEmail("");
-                setPassword("");
-            })
+        if (email !== "" && password != ""){
+            createUserWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                    // Signed in 
+                    console.log(userCredential)
+                    const user = userCredential.user;
+                    // ...
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    console.log(errorCode, errorMessage);
+                    // ..
+                })
+                .finally(()=> {
+                    setEmail("");
+                    setPassword("");
+                })
+        }
     }
 
     const handleLogin = () => {
-        signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                // Signed in 
-                const user = userCredential.user;
-                console.log(user);
-                // ...
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.log(errorCode, errorMessage);
-            })
-            .finally(()=> {
-                setEmail("");
-                setPassword("");
-            })
+        if (email !== "" && password != ""){
+            signInWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                    // Signed in 
+                    const user = userCredential.user;
+                    console.log(user);
+                    // ...
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    console.log(errorCode, errorMessage);
+                })
+                .finally(()=> {
+                    setEmail("");
+                    setPassword("");
+                })
+        }
     }
 
     return (
